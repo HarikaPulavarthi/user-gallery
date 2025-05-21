1d1561e5a7585f758928399127397c3180225f2a
Harika Pulavarthi <pulavarthiharika@gmail.com>
2025-05-21 07:10:54 +0530

    Initial commit

---
+++ src/main/java/com/synchrony/userGallery/UserGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.synchrony.userGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synchrony/userGallery/UserGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synchrony.userGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
